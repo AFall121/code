76489cc45467cfc75b34ed0ce530006ad0eee9b4
LuoYe <751498096@qq.com>
2020-05-26 14:14:00 +0800

    学习react
    学

---
+++ LearnFromB站/LearnFrom米修在线/Web前端开发MVVM框架React经典课程/my-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <div>this is header</div>
+    </div>
+    // <div>只能有一个</div>
+  );
+}
+
+export default App;
---
+++ ReactNote.md
@@ -0,0 +1,7 @@
+# 创建一个react应用
+- 1.npx create-react-app my-app
+![avatar](./learnImages/react1.jpg)
+- 2.根据提示 cd my-app 然后 yarn start
+![avatar](./learnImages/react2.jpg)
+- 3.比较重要的文件index.html index.js
+![avatat](./learnImages/react3.jpg)
