2e2ea8f55665f857dab78aeee859eaa28450f94e
LuoYe <751498096@qq.com>
2020-09-05 00:08:41 +0800

    some forget submit
    some forget submit

---
+++ vue-pactice/CJ/inventory/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <v-navigation-drawer dark color="orange" drawer="drawer">
+
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => ({
+    drawer: true
+  })
+}
+</script>
---
+++ vue-pactice/CJ/inventory/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h2>home</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
